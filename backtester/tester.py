7586ecd22dc38d0608fe221fda2567e85e74dfa2
JakeL1001 <me@jakelyell.dev>
2022-03-13 16:40:41 +0800

    added charting

--- backtester/tester.py
+++ backtester/tester.py
@@ -8,7 +8,7 @@ from functools import partial
 # Function used to backtest each stock
 # Parameters: stock - the name of the stock data csv to be tested
 #             logic - the logic function to be used
-def backtest_stock(results, stock, logic):
+def backtest_stock(results, stock, logic, chart):
     lock = mp.Lock() # Lock used to prevent errors with multiprocessing
     df = pd.read_csv("data/" + stock + ".csv", parse_dates=[0]) # Read the csv file into a dataframe to be tested
     backtest = engine.backtest(df) # Create a backtest object with the data from the csv
@@ -18,18 +18,20 @@ def backtest_stock(results, stock, logic):
     data.extend([stock]) # Add the stock name to the results for easy comparison
     results.append(data) # Add the results to the list of results
     lock.release()
+    if chart == True:
+        backtest.chart() # Chart the results
     return data # Return the results
 
 # Function used to test an array of stocks
 # Parameters: arr - the array of stock data csv's to be tested
 #             logic - the logic function to be used
 
-def testArr(arr, logic):
+def testArr(arr, logic, chart):
     manager = mp.Manager() # Create a multiprocessing manager
     results = manager.list() # Create a list to store the results
     processes = [] 
     for stock in arr: # For each stock in the array
-        p = mp.Process(target=backtest_stock, args=(results, stock, logic)) # Create a process to backtest each stock
+        p = mp.Process(target=backtest_stock, args=(results, stock, logic, chart)) # Create a process to backtest each stock
         processes.append(p) # Add the process to the list of processes
         p.start() # Start the process
     for process in processes: 
--- mean_reversion_example.py
+++ mean_reversion_example.py
@@ -7,22 +7,32 @@ from backtester import engine, tester
 from backtester import API_Interface as api
 
 def logic(account, lookback): # Logic function to be used for each time interval in backtest 
-    training_period = 2
+    training_period = 20
     try:
         today = len(lookback)-1
-        if(today > training_period): 
-            price_moving_average = lookback['close'].rolling(window=training_period).mean()[today]  # update PMA
-            volumn_moving_average = lookback['volume'].rolling(window=training_period).mean()[today]  # update VMA
+        if(today > training_period):
+            # If df['BUY'] is True, buy stock
+            if(lookback['BUY'][today]):
+                if(account.buying_power > 0):
+                    account.enter_position('long', account.buying_power, lookback['close'][today])
+            # If df['SELL'] is True, sell stock
+            if(lookback['SELL'][today]):
+                # for position in account.positions:
+                    account.enter_position('short', account.buying_power, lookback['close'][today])
+                    # account.close_position(position, 1, lookback['close'][today])
 
-            if(lookback['close'][today] < price_moving_average):
-                if(lookback['volume'][today] > volumn_moving_average):
-                    if(account.buying_power > 0):
-                        account.enter_position('long', account.buying_power, lookback['close'][today])
-            else:
-                if(lookback['close'][today] > price_moving_average):
-                    if(lookback['volume'][today] < volumn_moving_average):
-                        for position in account.positions:
-                                account.close_position(position, 1, lookback['close'][today]) 
+        #     price_moving_average = lookback['close'].rolling(window=training_period).mean()[today]  # update PMA
+        #     volumn_moving_average = lookback['volume'].rolling(window=training_period).mean()[today]  # update VMA
+
+        #     if(lookback['close'][today] < price_moving_average):
+        #         if(lookback['volume'][today] > volumn_moving_average):
+        #             if(account.buying_power > 0):
+        #                 account.enter_position('long', account.buying_power, lookback['close'][today])
+        #     else:
+        #         if(lookback['close'][today] > price_moving_average):
+        #             if(lookback['volume'][today] < volumn_moving_average):
+        #                 for position in account.positions:
+        #                         account.close_position(position, 1, lookback['close'][today]) 
     except Exception as e:
         print(e)
         pass  # Handles lookback errors in beginning of dataset
@@ -34,8 +44,10 @@ def preprocessData(list_of_stocks):
         df['TP'] = (df['close'] + df['low'] + df['high'])/3 # Calculate Typical Price
         df['std'] = df['TP'].rolling(20).std(ddof=0) # Calculate Standard Deviation
         df['MA-TP'] = df['TP'].rolling(20).mean() # Calculate Moving Average of Typical Price
-        df['BOLU'] = df['MA-TP'] + 2*df['std'] # Calculate Upper Bollinger Band
-        df['BOLD'] = df['MA-TP'] - 2*df['std'] # Calculate Lower Bollinger Band
+        df['BOLU'] = df['MA-TP'] + 1*df['std'] # Calculate Upper Bollinger Band
+        df['BOLD'] = df['MA-TP'] - 1*df['std'] # Calculate Lower Bollinger Band
+        df["BUY"] = df['close'].gt(df['BOLU']) # Calculate if price is above upper band
+        df["SELL"] = df['close'].lt(df['BOLD']) # Calculate if price is below lower band
         df.to_csv("data/" + stock + "_Processed.csv", index=False) # Save to CSV
         list_of_stocks_processed.append(stock + "_Processed")
     return list_of_stocks_processed
@@ -45,7 +57,7 @@ if __name__ == "__main__":
     list_of_stocks = ["TSLA_2020-03-09_2022-01-28_15min"] # List of stock data csv's to be tested, located in "data/" folder 
     list_of_stocks_proccessed = preprocessData(list_of_stocks) # Preprocess the data
     starttime = time.time() # Start timer
-    results = tester.testArr(list_of_stocks_proccessed, logic) # Run backtest on list of stocks using the logic function
+    results = tester.testArr(list_of_stocks_proccessed, logic, chart=True) # Run backtest on list of stocks using the logic function
 
     print(results) # Print results
     df = pd.DataFrame(list(results),columns=["Buy and Hold","Strategy","Longs","Sells","Shorts","Covers","Stdev_Strategy","Stdev_Hold","Coin"]) # Create dataframe of results
