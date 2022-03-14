1feeb80ee44360f19e11b9b211e002706ef847e7
JakeL1001 <me@jakelyell.dev>
2022-03-14 16:13:56 +0800

    Update to example

--- data_generator.py
+++ data_generator.py
@@ -1,5 +1,5 @@
 from backtester import API_Interface as api
 
-df = api.get_intraday_extended('AAPL', 'all', '', '1min', True, False)
+df = api.get_intraday_extended('AAPL', 'all', '', '15min', True)
 # df = api.get_intraday_extended('TSLA', 'year1month2', 'year1month1', '60min', True, False)
 # api.get_intraday_extended('TSLA', 'all', 'year1month1', '15min', True, True)
--- mean_reversion_example.py
+++ mean_reversion_example.py
@@ -6,21 +6,21 @@ import multiprocessing as mp
 from backtester import engine, tester
 from backtester import API_Interface as api
 
-training_period = 30 # 20 for demo
-standard_deviations = 3.5
+training_period = 20 # 20 for demo
+standard_deviations = 3.5 # 3.5 for demo
 
 def logic(account, lookback): # Logic function to be used for each time interval in backtest 
     
     try:
         today = len(lookback)-1
         if(today > training_period):
-            # If df['BUY'] is True, buy stock
+            # If df['BUY'] is True, buy/cover stock
             if(lookback['BUY'][today]):
                 for position in account.positions:
                     account.close_position(position, 1, lookback['close'][today])
                 if(account.buying_power > 0):
                     account.enter_position('long', account.buying_power, lookback['close'][today])
-            # If df['SELL'] is True, sell stock
+            # If df['SELL'] is True, sell/short stock
             if(lookback['SELL'][today]):
                 for position in account.positions:
                     account.close_position(position, 1, lookback['close'][today])
@@ -30,15 +30,15 @@ def logic(account, lookback): # Logic function to be used for each time interval
         print(e)
         pass  # Handles lookback errors in beginning of dataset
 
-def preprocessData(list_of_stocks):
+def preprocess_data(list_of_stocks):
     list_of_stocks_processed = []
     for stock in list_of_stocks:
         df = pd.read_csv("data/" + stock + ".csv", parse_dates=[0])
         df['TP'] = (df['close'] + df['low'] + df['high'])/3 # Calculate Typical Price
         df['std'] = df['close'].rolling(training_period).std(ddof=0) # Calculate Standard Deviation
         df['MA-TP'] = df['TP'].rolling(training_period).mean() # Calculate Moving Average of Typical Price
-        df['BOLU'] = df['MA-TP'] + standard_deviations*df['std'] # Calculate Upper Bollinger Band (3.5 for demo)
-        df['BOLD'] = df['MA-TP'] - standard_deviations*df['std'] # Calculate Lower Bollinger Band (3.5 for demo)
+        df['BOLU'] = df['MA-TP'] + standard_deviations*df['std'] # Calculate Upper Bollinger Band
+        df['BOLD'] = df['MA-TP'] - standard_deviations*df['std'] # Calculate Lower Bollinger Band
         df["BUY"] = df['close'].gt(df['BOLU']) # Calculate if price is above upper band
         df["SELL"] = df['close'].lt(df['BOLD']) # Calculate if price is below lower band
         df.to_csv("data/" + stock + "_Processed.csv", index=False) # Save to CSV
@@ -46,14 +46,14 @@ def preprocessData(list_of_stocks):
     return list_of_stocks_processed
 
 if __name__ == "__main__":
-    list_of_stocks = ["TSLA_2020-03-01_2022-01-20_1min"] 
-    # list_of_stocks = ["TSLA_2020-03-09_2022-01-28_15min"] # List of stock data csv's to be tested, located in "data/" folder 
-    list_of_stocks_proccessed = preprocessData(list_of_stocks) # Preprocess the data
+    # list_of_stocks = ["TSLA_2020-03-01_2022-01-20_1min"] 
+    list_of_stocks = ["TSLA_2020-03-09_2022-01-28_15min", "AAPL_2020-03-24_2022-02-12_15min"] # List of stock data csv's to be tested, located in "data/" folder 
+    list_of_stocks_proccessed = preprocess_data(list_of_stocks) # Preprocess the data
     starttime = time.time() # Start timer
-    results = tester.testArr(list_of_stocks_proccessed, logic, chart=True) # Run backtest on list of stocks using the logic function
+    results = tester.test_array(list_of_stocks_proccessed, logic, chart=True) # Run backtest on list of stocks using the logic function
 
-    print("training period" + str(training_period))
-    print("standard deviations" + str(standard_deviations))
+    print("training period " + str(training_period))
+    print("standard deviations " + str(standard_deviations))
     print(results) # Print results
     df = pd.DataFrame(list(results),columns=["Buy and Hold","Strategy","Longs","Sells","Shorts","Covers","Stdev_Strategy","Stdev_Hold","Coin"]) # Create dataframe of results
     df.to_csv("resultsoftesting.csv",index =False) # Save results to csv
